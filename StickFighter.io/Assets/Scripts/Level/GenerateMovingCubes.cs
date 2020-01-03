b2297851d2cc0a8e33558ef2eec14f260300bac9
marie.gallardo <marie.gallardo@efrei.net>
2020-01-03 12:30:38 +0100

    Transfer from Unity repo to git repo

---
+++ StickFighter.io/Assets/Scripts/Level/GenerateMovingCubes.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class GenerateMovingCubes : MonoBehaviour
+{
+    public int nbrWantedCubes;
+    public GameObject prefab;
+
+    private int currentCubes = 0;
+    protected Vector3 startingPosition = new Vector3(0, 0, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnCube();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(currentCubes < nbrWantedCubes)
+        {
+            spawnCube();
+            currentCubes++;
+        }
+    }
+
+    protected virtual Vector2 GetMovingForce()
+    {
+        return transform.right * 500;
+    }
+
+    protected virtual Vector3 GetStartingPosition()
+    {
+        int height = Random.Range(-1, 1);
+
+        var actualStartingPos = startingPosition;
+        actualStartingPos.y = height * 2;
+        return actualStartingPos;
+    }
+
+    public void spawnCube()
+    {
+        var obj = Instantiate(prefab, GetStartingPosition(), Quaternion.identity);
+        obj.tag = "Moving-Cube";
+        var rb = obj.gameObject.AddComponent<Rigidbody2D>();
+        rb.AddForce(GetMovingForce());
+        rb.gravityScale = 0;
+    }
+}
---
+++ StickFighter.io/Assets/Scripts/Level/HorizontalCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalCube : GenerateMovingCubes
+{
+    protected new Vector3 startingPosition = new Vector3(-22, 0, 0);
+
+    protected override Vector2 GetMovingForce()
+    {
+        return transform.right * 500;
+    }
+
+    protected override Vector3 GetStartingPosition()
+    {
+        int height = Random.Range(-1, 1);
+
+        var actualStartingPos = startingPosition;
+        actualStartingPos.y = height * 2;
+        return actualStartingPos;
+    }
+}
